9322b6c1e00ac839597911afc052b50b66a9bb04
CYoung5233 <cyoung17@uw.edu>
2025-05-04 16:12:44 -0700

    Handles arg parsing and sharing better

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import gymnasium as gym
 import numpy as np
+import argparse
+from argparse import Namespace
 from agents.dqn_agent import DQNAgent
 from utils.train_logger import TrainLogger
 from typing import SupportsFloat, Tuple, Any
@@ -8,8 +10,8 @@ from tqdm import tqdm
 
 def train(env: gym.Env, agent: DQNAgent, n_episodes: int = 1000, max_t: int = 1000,
           eps_start: float = 1.0, eps_end: float = 0.01, eps_decay: float = 0.995,
-          render_last: bool = False) -> list[float]:
-    logger = TrainLogger(printer=tqdm.write)
+          render_last: bool = False, double_flag: bool = False) -> list[float]:
+    logger = TrainLogger(printer=tqdm.write, double_flag=double_flag)
     eps = eps_start
 
     for i_episode in tqdm(range(1, n_episodes + 1)):
@@ -41,13 +43,26 @@ def train(env: gym.Env, agent: DQNAgent, n_episodes: int = 1000, max_t: int = 10
     logger.save_metrics()
     return logger.scores
 
+# Parses arguments
+def parse_args() -> Namespace:
+    parser = argparse.ArgumentParser(description="TCSS 435 Lunar Lander")
+    parser.add_argument("--double", action="store_true",
+                        help="Use double DQN instead of single DQN. Defaults to single.")
+    return parser.parse_args()
 
-if __name__ == "__main__":
-    env = gym.make("LunarLander-v3")
-    state_size = env.observation_space.shape[0] # type: ignore
-    action_size = env.action_space.n # type: ignore
+# Main method
+def main() -> None:
+    args: Namespace = parse_args()
+    double_flag: bool = args.double
+    env: gym.Env = gym.make("LunarLander-v3")
+    state_size: int = env.observation_space.shape[0] # type: ignore
+    action_size: int = env.action_space.n # type: ignore
 
-    agent = DQNAgent(state_size, action_size, use_double_dqn=True)
-    scores = train(env, agent, render_last=True)
+    agent: DQNAgent = DQNAgent(state_size, action_size, double_flag=double_flag)
+    print(f"{'Double DQN' if double_flag else 'Single DQN'} is being used.")
+    scores: list[float] = train(env, agent, render_last=True, double_flag=double_flag)
 
     env.close()
+
+if __name__ == "__main__":
+    main()
